d1c29081613228455e3ff4734e5a219e9e961122
Booster39 <abdiame10@yahoo.fr>
2024-08-21 09:13:36 +0200

    feat: set up Java Spring boot backend project

---
+++ back/sickvisit/sickvisit/src/test/java/com/example/sickvisit/SickvisitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sickvisit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SickvisitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
